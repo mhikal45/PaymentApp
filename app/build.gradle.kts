57175ee857916e96a2dbc75148e321d40ea1b603
muhammad hikal <muhammadhikal12@gmail.com>
2024-09-11 01:18:18 +0700

    unit test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -22,6 +24,12 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
+        }
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
         }
     }
 
@@ -105,11 +113,21 @@ dependencies {
     implementation(libs.converter.moshi)
     implementation(libs.logging.interceptor)
 
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
+    testImplementation(platform(libs.androidx.compose.bom))
+    testImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.androidx.junit)
+    testImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.kotlinx.coroutines.test.v181)
+    androidTestImplementation(libs.ui.test.junit4)
+    androidTestImplementation(libs.androidx.rules)
+    androidTestImplementation(libs.androidx.runner)
+
+
+
 }
--- app/src/test/java/com/mmi/paymentapp/ExampleUnitTest.kt
+++ app/src/test/java/com/mmi/paymentapp/ExampleUnitTest.kt
@@ -1,26 +1,76 @@
 package com.mmi.paymentapp
 
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
+import android.content.Context
+import androidx.compose.runtime.mutableStateOf
+import androidx.room.Room.inMemoryDatabaseBuilder
+import androidx.test.core.app.ApplicationProvider
+import com.mmi.paymentapp.data.local.PaymentAppDatabase
+import com.mmi.paymentapp.data.local.PromotionDao
+import com.mmi.paymentapp.data.local.TransactionDao
+import com.mmi.paymentapp.data.model.Transaction
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
-import org.junit.Rule
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+@RunWith(RobolectricTestRunner::class)
 class ExampleUnitTest {
-    @get:Rule
-    val composeTestRule = createComposeRule()
+
+    private lateinit var transactionDao: TransactionDao
+    private lateinit var promotionDao: PromotionDao
+    private lateinit var paymentAppDatabase: PaymentAppDatabase
+
+    @Before
+    fun setUp() {
+        paymentAppDatabase = inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext<Context>(),
+            PaymentAppDatabase::class.java
+        )
+            .allowMainThreadQueries()
+            .build()
+        transactionDao = paymentAppDatabase.PaymentAppDao
+        promotionDao = paymentAppDatabase.PromotionDao
+    }
+
+    @After
+    fun tearDown() {
+        paymentAppDatabase.close()
+    }
 
     @Test
-    fun HomeTabFunction(){
-        //Home Tab
-        composeTestRule.setContent {
-            composeTestRule.onNodeWithText("Sisa Saldo Anda").assertIsDisplayed()
+    fun `paymentDaoTest`() = runTest {
+        val zoneId = ZoneId.of("GMT+7")
+        val newTransaction = Transaction(
+            balance = 1000000F,
+            channel = "",
+            provider = "",
+            paymentInfo = "",
+            type = "Topup",
+            merchant = "Payment",
+            amount = 1000000F,
+            createdDate = ZonedDateTime.now(zoneId).toString()
+        )
+
+        val transactionList = mutableListOf(newTransaction)
+        var latestTransaction : Transaction? = null
+        var localTransactionList = mutableListOf<Transaction>()
+
+            transactionDao.InsertTransaction(newTransaction)
+        transactionDao.GetLatestTransaction().collect{
+            latestTransaction = it
         }
+        transactionDao.GetAllTransaction().collect{
+            localTransactionList = it as MutableList<Transaction>
+        }
+
+        assertEquals(latestTransaction!!,newTransaction)
+        assertEquals(localTransactionList,transactionList)
     }
 }
